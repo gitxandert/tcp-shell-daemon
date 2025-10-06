129f02243aafb409883a50ed2880e18ef7c49eab
gitxandert <alexandertoth96@yahoo.com>
2025-10-06 18:31:47 -0400

    move functions to separate file

---
+++ vm_lru_hash_radix.c
@@ -0,0 +1,174 @@
+#include "vm_lru_hash_radix.h"
+
+// we need:
+// - locks
+// - init functions   V
+// - hash algorithms  V
+// - insert into list V
+// - insert into hash
+// - insert into tree
+// - look up hash     V
+// - look up tree     V
+// - remove from list V
+// - remove from hash
+// - remove from tree
+//
+// So here's the flow:
+// 1. Client opens file.
+// 2. Prebuffer adds n pages from the buffer, either from VM or disk.
+// 3. Feed pages from buffer into audio stream (can use ALSA for this).
+// 4. Fill buffer up to a certain point, then wait until stream requests more.
+//
+// Okay, for now, let's just implement the insert functions.
+
+// assume global list
+vm_list_t VM_LIST;
+
+void vm_list_init() {
+  VM_LIST.head = VM_LIST.tail = NULL;
+  VM_LIST.size = 0;
+  VM_LIST.map = malloc(sizeof(hashmap_entry_t *) * HASH_BUCKETS_CAPACITY);
+}
+
+// hashmap_t functions
+//
+// hash with inode and page index (i.e. with what would be stored in vm_page_t)
+size_t hash_page(ino_t key, uint64_t page_index) {
+  uint64_t h = (uint64_t)key * 11400714819323198485llu; // Knuth constant
+  h ^= page_index + 0x9e3779b97f4a7c15llu + (h << 6) + (h >> 2);
+  return (size_t)(h % HASH_BUCKETS_CAPACITY);
+}
+
+void delete_hashmap_entry(ino_t inode, uint64_t page_index) {
+  size_t hash = hash_page(inode, page_index);
+
+  if (VM_LIST.map->buckets[hash]) {
+    hashmap_entry_t *tmp = VM_LIST.map->buckets[hash];
+    // sift through entry chains until key matches or end of chain
+    while (tmp->key != inode && tmp->next)
+      tmp = tmp->next;
+
+    if (tmp->key == inode) {
+      if (tmp->next)
+        tmp = tmp->next;
+      else
+        tmp = NULL;
+    }
+
+    // if at end of chain and key doesn't match, do nothing
+  }
+}
+//
+// end hashmap_t functions
+
+// radix_tree_t functions
+//
+size_t get_radix_byte(uint64_t page_index, int level) {
+  int shift = (12 - level) * 4;
+
+  return (size_t)(page_index >> shift & 0xF);
+}
+
+bool get_page(radix_tree_t *tree, vm_page_t *ret_page, uint64_t page_index) {
+  radix_node_t *cur = tree->root;
+  int level = 0;
+  // if there's no vm_page_t stored at the current node
+  // then check the slots to see if anything is stored at the next level
+  while (cur && !cur->page) {
+    size_t cur_byte = get_radix_byte(page_index, level);
+    if (cur->slots[cur_byte]) // go to the next level
+      cur = cur->slots[cur_byte];
+    else // the next level at this index is empty
+      return false;
+
+    level++;
+  }
+
+  if (cur && cur->page && cur->page->page_index == page_index) {
+    ret_page = cur->page;
+    return true;
+  }
+
+  return false;
+}
+//
+// end radix_tree_t functions
+
+// vm_list_t functions
+//
+// moves a (new or existing) page to the head
+void push_to_head(vm_page_t* new_head) {
+  if (VM_LIST.head == NULL) {
+    VM_LIST.head = new_head;
+    VM_LIST.tail = VM_LIST.head;
+  } else {
+    new_head->prev = NULL;
+    new_head->next = VM_LIST.head;
+    VM_LIST.head->prev = new_head;
+    VM_LIST.head = new_head;
+    // if this is the second page,
+    // VM_LIST.tail still points to previous head
+  }
+}
+
+// this moves existing nodes to the head
+void move_to_head(vm_page_t *new_head) {
+  // if this node is already the head of the list or NULL
+  if (VM_LIST.head == new_head || !new_head)
+   return;
+
+  // previous node points forward to next node
+  if (new_head->prev)
+   new_head->prev->next = new_head->next;
+  // next node points backward to previous node
+  if (new_head->next)
+    new_head->next->prev = new_head->prev;
+  else // if there is no next, then current node is the tail
+    VM_LIST.tail = new_head->prev;
+
+  // they are no longer pointing to the "current" node
+
+  // push new_head to head
+  push_to_head(new_head);
+}
+
+// remove tail if it and its prev exist
+void pop_tail() {
+  if (VM_LIST.tail && VM_LIST.tail->prev) {
+    VM_LIST.tail = VM_LIST.tail->prev;
+    VM_LIST.tail->next = NULL;
+  }
+}
+//
+// end vm_list_t functions
+
+// verify page is cached
+//
+bool is_in_memory(ino_t inode, uint64_t page_index) {
+  // if no hashmap_entry for this file, return
+  size_t hm_idx = hash_page(inode, page_index);
+  if (!VM_LIST.map[hm_idx])
+    return false;
+
+  hashmap_entry_t *hm_ent = VM_LIST.map[hm_idx];
+
+  // if inode not in hash, return false
+  while (hm_ent->key != inode && hm_ent->next != NULL)
+    hm_ent = hm_ent->next;
+  if (hm_ent->key != inode)
+    return false;
+
+  // if not in tree, return false
+  radix_tree_t *tree = &(hm_ent->file->tree);
+  if (tree->root == NULL)
+    return false;
+
+  vm_page_t *req_page = malloc(sizeof(vm_page_t *));
+  if (!get_page(tree, req_page, page_index))
+    return false;
+
+  // if page is in memory, move to front of LRU list
+  move_to_head(req_page);
+  return true;
+}
+
