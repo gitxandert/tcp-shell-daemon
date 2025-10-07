ef0973782a98a2948bead978baad915f99f9c5ef
gitxandert <alexandertoth96@yahoo.com>
2025-10-07 18:39:59 -0400

    add more insert functions

--- vm_lru_hash_radix.c
+++ vm_lru_hash_radix.c
@@ -39,12 +39,36 @@ size_t hash_page(ino_t key, uint64_t page_index) {
   return (size_t)(h % HASH_BUCKETS_CAPACITY);
 }
 
-void delete_hashmap_entry(ino_t inode, uint64_t page_index) {
+hashmap_entry_t *hashmap_entry_create(ino_t inode, uint64_t page_index) {
+  vm_page_t *new_page = malloc(sizeof(vm_page_t));
+  new_page->inode = inode;
+  new_page->page_index = page_index;
+  // set dirty and referenced later maybe, if I need them
+
+  radix_node_t *new_node = calloc(1, sizeof(radix_node_t));
+  new_node->page = new_page;
+
+  radix_tree_t new_tree;
+  new_tree.root = new_node;
+
+  vm_file_t *new_file = malloc(sizeof(vm_file_t));
+  new_file->inode = inode;
+  new_file->tree = new_tree; 
+
+  hashmap_entry_t *new_entry = malloc(sizeof(hashmap_entry_t));
+  new_entry->key = inode;
+  new_entry->file = new_file;
+  new_entry->next = NULL;
+
+  return new_entry;
+}
+
+void hashmap_entry_delete(ino_t inode, uint64_t page_index) {
   size_t hash = hash_page(inode, page_index);
 
   if (VM_LIST.map->buckets[hash]) {
     hashmap_entry_t *tmp = VM_LIST.map->buckets[hash];
-    // sift through entry chains until key matches or end of chain
+    // sift through entry chain until key matches or end of chain
     while (tmp->key != inode && tmp->next)
       tmp = tmp->next;
 
@@ -64,9 +88,54 @@ void delete_hashmap_entry(ino_t inode, uint64_t page_index) {
 // radix_tree_t functions
 //
 size_t get_radix_byte(uint64_t page_index, int level) {
-  int shift = (12 - level) * 4;
+  int shift = level * RADIX_BITS;
 
-  return (size_t)(page_index >> shift & 0xF);
+  return (size_t)(page_index << shift & 0xF);
+}
+
+void radix_manage_level(radix_node_t *node, int level) {
+  
+}
+
+void radix_insert(ino_t inode, uint64_t page_index) {
+  // - first hash,
+  //   then go to corresponding bucket
+  // - search for hashmap_entry_t with same inode
+  //   - if none exists, chain a new hashmap_entry_t,
+  //      then set inode to inode and create new vm_file_t 
+  //      (also setting its inode to inode)
+  // - for each level of the radix tree
+  //   - check if vm_page_t exists
+  //      - if the page exists, compare current level radix bytes
+  //         - if they're the same, then both pages need to drop down,
+  //            and the current vm_page_t has to be set to NULL
+  //            - might need a separate function for this; radix_move?
+  //              - seems recursive...
+  //      - if it doesn't exist, then check if any slots are full
+  //        - if yes, then bytes need to be compared
+  //          - if the page_index's byte at this level is the same as an
+  //            occupied slot's, then it'll need to drop down
+  size_t hash = hash_page(inode, page_index);
+  hashmap_entry_t *cur = VM_LIST.map->buckets[hash];
+
+  if (!cur) {
+    // if there is no hashmap entry for this file, make one
+    cur = hashmap_entry_create(inode, page_index);
+  else {
+    // find the entry in the collision chain that corresponds to the file
+    while (cur->key != inode && cur->next)
+      cur = cur->next;
+
+    if (cur->key != inode)
+      // end of chain, but there is no file stored in the bucket
+      cur->next = hashmap_entry_create(inode, page_index);
+    else {
+      // here is where we need some function that manages node insertions
+      // if there's a page stored at any level, it de facto needs to be moved down
+      radix_node_t *root = cur->file->tree.root;
+      radix_manage_level(root, 0);
+    }
+  }
 }
 
 bool get_page(radix_tree_t *tree, vm_page_t *ret_page, uint64_t page_index) {
@@ -97,7 +166,7 @@ bool get_page(radix_tree_t *tree, vm_page_t *ret_page, uint64_t page_index) {
 // vm_list_t functions
 //
 // moves a (new or existing) page to the head
-void push_to_head(vm_page_t* new_head) {
+void vm_list_push(vm_page_t* new_head) {
   if (VM_LIST.head == NULL) {
     VM_LIST.head = new_head;
     VM_LIST.tail = VM_LIST.head;
@@ -112,7 +181,7 @@ void push_to_head(vm_page_t* new_head) {
 }
 
 // this moves existing nodes to the head
-void move_to_head(vm_page_t *new_head) {
+void vm_list_move_to_head(vm_page_t *new_head) {
   // if this node is already the head of the list or NULL
   if (VM_LIST.head == new_head || !new_head)
    return;
@@ -129,11 +198,11 @@ void move_to_head(vm_page_t *new_head) {
   // they are no longer pointing to the "current" node
 
   // push new_head to head
-  push_to_head(new_head);
+  vm_list_push(new_head);
 }
 
 // remove tail if it and its prev exist
-void pop_tail() {
+void vm_list_pop() {
   if (VM_LIST.tail && VM_LIST.tail->prev) {
     VM_LIST.tail = VM_LIST.tail->prev;
     VM_LIST.tail->next = NULL;
@@ -163,12 +232,12 @@ bool is_in_memory(ino_t inode, uint64_t page_index) {
   if (tree->root == NULL)
     return false;
 
-  vm_page_t *req_page = malloc(sizeof(vm_page_t *));
+  vm_page_t *req_page = malloc(sizeof(vm_page_t));
   if (!get_page(tree, req_page, page_index))
     return false;
 
   // if page is in memory, move to front of LRU list
-  move_to_head(req_page);
+  vm_list_move_to_head(req_page);
   return true;
 }
 
